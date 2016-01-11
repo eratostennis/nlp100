9952447b8019d2d8442169dba12b3a3c1f307129
eratostennis <eiji.sekiya.0326@gmail.com>
2016-01-12 02:26:29 +0900

    exercise zip and zipWithIndex (nlp 01,02)

--- src/main/scala/nlp100/Section1.scala
+++ src/main/scala/nlp100/Section1.scala
@@ -4,3 +4,24 @@ object p0 {
     println(str.reverse)
   }
 }
+
+object p1 {
+  val str = "パタトクカシーー"
+  def main(args: Array[String]) {
+    // use collect(/map) with index => zipWithIndex
+    val ans = str.zipWithIndex.collect {
+      case (word, index) if (index % 2 != 0) => word
+    }.mkString
+    println(ans)
+  }
+}
+
+object p2 {
+  val str1 = "パトカー"
+  val str2 = "タクシー"
+  def main(args: Array[String]) {
+    // aggregate the contents of two lists into a single list of pairs 
+    val ans = str1.zip(str2).flatMap { w => Seq(w._1, w._2) }.mkString
+    println(ans)
+  }
+}
