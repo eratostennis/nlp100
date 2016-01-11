b219e927af806fcbc80940fb58aa3efb5507a046
eratostennis <eiji.sekiya.0326@gmail.com>
2016-01-11 12:21:26 +0900

    First commit (Section1-00)

---
+++ build.sbt
@@ -0,0 +1,6 @@
+
+name := "nlp100"
+
+version := "0.1"
+
+scalaVersion := "2.10.5"
---
+++ src/main/scala/nlp100/Section1.scala
@@ -0,0 +1,6 @@
+object p0 {
+  val str = "stressed"
+  def main(args: Array[String]) {
+    println(str.reverse)
+  }
+}
