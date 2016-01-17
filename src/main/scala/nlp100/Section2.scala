d109158bc3d6dee30ab54847d765c5fe064ecae3
eratostennis <eiji.sekiya.0326@gmail.com>
2016-01-17 17:23:07 +0900

    Use replace and PrintWriter (11,12)

--- src/main/scala/nlp100/Section2.scala
+++ src/main/scala/nlp100/Section2.scala
@@ -1,7 +1,29 @@
 import scala.io.Source
+import java.io.PrintWriter
 
 object p10 {
   def main(args: Array[String]) {
     println(Source.fromInputStream(getClass.getResourceAsStream("/hightemp.txt")).getLines.size)
+  }
+}
+
+object p11 {
+  val str = "replace	one	tab	to	one	space"
+  def main(args: Array[String]) {
+    println(str.replaceAll("\t", " "))
+  }
+}
+
+object p12 {
+  def main(args: Array[String]) {
+    val col1 = new PrintWriter("col1.txt")
+    val col2 = new PrintWriter("col2.txt")
+    Source.fromInputStream(getClass.getResourceAsStream("/hightemp.txt")).getLines().foreach { line =>
+      val words = line.split("\t")
+      col1.write(words(0)+"\n")
+      col2.write(words(1)+"\n")
+    }
+    col1.close()
+    col2.close()
   }
 }
